00df8dcdc7ccf6619d8505bf73838e57e1432067
Gejgalis, Piotr <piotr@gejgalis.com>
2014-11-30 10:00:58 +0100

    added topics for tests

--- src/es6/main/main.js
+++ src/es6/main/main.js
@@ -8,7 +8,10 @@ new Vue({
     data: {
         tests: [
             {id: "vector-features", label: "Vector Features", selected: false},
-            {id: "overlay-elements", label: "Overlay Elements", selected: false}
+            {id: "overlay-elements", label: "Overlay Elements", selected: false},
+            {id: "animating-features", label: "Animating Features", selected: false},
+            {id: "hit-detection", label: "Hit Detection", selected: false},
+            {id: "topo-json", label: "TopoJSON", selected: false}
         ],
         selectedTest: {}
     },
